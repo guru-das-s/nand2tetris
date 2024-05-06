18ea10a34b3ea1b0ac8ae6c2bb5935a671826539
Guru Das Srinagesh <gurooodas@gmail.com>
2024-05-06 00:09:30 -0700

    n2t: Add Mult.asm
    Project 4 first part
    
    Signed-off-by: Guru Das Srinagesh <gurooodas@gmail.com>

--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -7,4 +7,30 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+@i
+M=0    // i = 0
+
+@R2
+M=0    // Initialize R2 to 0
+
+(LOOP)
+@i
+D=M
+@R1
+D=D-M  // D = (i - M[R1])
+@END
+D;JGE  // We're done
+
+@R0
+D=M
+@R2
+M=M+D
+
+@i
+M=M+1  // i++
+@LOOP
+0;JMP
+
+(END)
+@END
+0;JMP
