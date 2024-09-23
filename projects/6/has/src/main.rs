06e49f0a1eb6e242e6a83f4cf24c26c6602bf910
Guru Das Srinagesh <gurooodas@gmail.com>
2024-09-22 21:37:21 -0700

    n2t: has: Add dummy to_binary code scaffolding
    Signed-off-by: Guru Das Srinagesh <gurooodas@gmail.com>

--- projects/6/has/src/main.rs
+++ projects/6/has/src/main.rs
@@ -1,13 +1,14 @@
 use clap::Parser;
 use std::error::Error;
 use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
+use std::io::{self, BufRead, BufWriter, Write};
+use std::path::{Path, PathBuf};
 
 mod parser;
 mod spec;
+mod to_binary;
 
-#[derive(Parser, Debug)]
+#[derive(Default, Parser, Debug)]
 /// Hack assembler
 ///
 /// Converts Hack assembly files to Hack binary
@@ -15,16 +16,31 @@ mod spec;
 struct Args {
     /// Path to Hack ASM file (e.g. Max.asm)
     #[arg(short, long)]
-    filename: String,
+    filename: PathBuf,
+
+    /// Output filename (e.g. Max.hack)
+    #[arg(short, long)]
+    output: Option<PathBuf>,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let args = Args::parse();
+    let mut args = Args::parse();
+
+    if args.output.is_none() {
+        args.output = Some(args.filename.with_extension("hack"));
+    }
+
+    let output_file = File::create(args.output.unwrap())?;
+    let mut writer = BufWriter::new(output_file);
+
     let lines = read_lines(args.filename)?;
 
     for line in lines.flatten() {
         let parsed_line = parser::HackLine::parse_line(&line)?;
         println!("{} -> {:?}", line, parsed_line);
+        if let Some(bincode) = to_binary::binary_of(parsed_line) {
+            writeln!(writer, "{}", bincode)?;
+        }
     }
 
     Ok(())
---
+++ projects/6/has/src/to_binary.rs
@@ -0,0 +1,6 @@
+use crate::parser::HackLine;
+use crate::spec::{Comp, Destination, Jump};
+
+pub fn binary_of(line: HackLine) -> Option<String> {
+    Some("xxx".to_string())
+}
