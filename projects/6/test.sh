6356119c323ec3ccb5634d6c8ff518fd8b31d73e
Guru Das Srinagesh <gurooodas@gmail.com>
2024-10-06 00:22:23 -0700

    n2t: has: Add test script
    Running this script from the repo root directory proves that the `has`
    assembler matches the stock Assembler.sh's output for all test asm
    files.
    
    ```
    $ ./projects/6/test.sh && echo "No diffs!"
    Assembling /home/gurus/projects/nand2tetris/projects/6/rect/Rect.asm
    Assembling /home/gurus/projects/nand2tetris/projects/6/rect/RectL.asm
    Assembling /home/gurus/projects/nand2tetris/projects/6/max/Max.asm
    Assembling /home/gurus/projects/nand2tetris/projects/6/max/MaxL.asm
    Assembling /home/gurus/projects/nand2tetris/projects/6/add/Add.asm
    Assembling /home/gurus/projects/nand2tetris/projects/6/pong/Pong.asm
    Assembling /home/gurus/projects/nand2tetris/projects/6/pong/PongL.asm
        Finished `release` profile [optimized] target(s) in 0.06s
         Running `target/release/has -f projects/6/rect/Rect.asm -o projects/6/rect/Rect.has.hack`
        Finished `release` profile [optimized] target(s) in 0.02s
         Running `target/release/has -f projects/6/rect/RectL.asm -o projects/6/rect/RectL.has.hack`
        Finished `release` profile [optimized] target(s) in 0.02s
         Running `target/release/has -f projects/6/max/Max.asm -o projects/6/max/Max.has.hack`
        Finished `release` profile [optimized] target(s) in 0.02s
         Running `target/release/has -f projects/6/max/MaxL.asm -o projects/6/max/MaxL.has.hack`
        Finished `release` profile [optimized] target(s) in 0.02s
         Running `target/release/has -f projects/6/add/Add.asm -o projects/6/add/Add.has.hack`
        Finished `release` profile [optimized] target(s) in 0.02s
         Running `target/release/has -f projects/6/pong/Pong.asm -o projects/6/pong/Pong.has.hack`
        Finished `release` profile [optimized] target(s) in 0.02s
         Running `target/release/has -f projects/6/pong/PongL.asm -o projects/6/pong/PongL.has.hack`
    No diffs!
    ```
    
    Signed-off-by: Guru Das Srinagesh <gurooodas@gmail.com>

---
+++ projects/6/test.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Test the `has` assembler
+# 	Has to be executed from the repo's root folder.
+
+cleanup() {
+	find projects/6/ -iname *.hack -exec rm {} \;
+}
+
+trap cleanup EXIT
+
+run_has() {
+	local asm_file="$1"
+	local has_hack_file="${asm_file%.asm}.has.hack"
+	cargo run --release --bin has -- -f $asm_file -o $has_hack_file
+}
+
+diff_output() {
+	local asm_file="$1"
+	local has_hack_file="${asm_file%.asm}.has.hack"
+	local ground_truth_file="${asm_file%.asm}.hack"
+
+	diff -q $has_hack_file $ground_truth_file
+}
+
+# First, generate ground truth files from provided Assembler.sh
+find projects/6/ -iname *.asm -exec ./tools/Assembler.sh {} \;
+
+find projects/6/ -iname *.asm | while read -r file; do
+	# Run `has` on the same asm files as above
+	run_has $file
+
+	# Compare with ground truth files
+	diff_output $file
+done
