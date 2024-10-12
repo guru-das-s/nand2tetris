13f23dfa84c74abf7aea3beabf15adc92d866385
Guru Das Srinagesh <gurooodas@gmail.com>
2024-10-12 01:22:55 -0700

    n2t: has: Add better error handling
    In case there is a parse error, clean up after yourself by deleting the
    output file.
    
    Signed-off-by: Guru Das Srinagesh <gurooodas@gmail.com>

--- projects/6/has/src/main.rs
+++ projects/6/has/src/main.rs
@@ -26,6 +26,34 @@ struct Args {
     output: Option<PathBuf>,
 }
 
+fn assemble(
+    writer: &mut BufWriter<File>,
+    lines: &Vec<String>,
+    symbol_table: &mut SymbolTable,
+) -> Result<(), Box<dyn Error>> {
+    let mut instr_num: i16 = -1;
+
+    for line in lines {
+        let parsed_line = HackLine::parse_line(&line)?;
+        match parsed_line {
+            HackLine::A { .. } | HackLine::C { .. } | HackLine::Variable { .. } => instr_num += 1,
+            HackLine::Label { label } => symbol_table.add_new_label(label, instr_num),
+            _ => {}
+        }
+    }
+
+    for line in lines {
+        let parsed_line = HackLine::parse_line(&line)?;
+        #[cfg(debug_assertions)]
+        println!("{} -> {:?}", line, parsed_line);
+        if let Some(bincode) = to_binary::binary_of(parsed_line, symbol_table) {
+            writeln!(writer, "{}", bincode)?;
+        }
+    }
+
+    Ok(())
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let mut args = Args::parse();
 
@@ -35,28 +63,14 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let mut symbol_table = SymbolTable::init();
 
-    let output_file = File::create(args.output.unwrap())?;
+    let output_file = File::create(args.output.as_ref().unwrap())?;
     let mut writer = BufWriter::new(output_file);
 
     let lines: Vec<_> = read_lines(args.filename)?.collect::<Result<_, _>>()?;
 
-    let mut instr_num: i16 = -1;
-    for line in &lines {
-        let parsed_line = HackLine::parse_line(&line)?;
-        match parsed_line {
-            HackLine::A { .. } | HackLine::C { .. } | HackLine::Variable { .. } => instr_num += 1,
-            HackLine::Label { label } => symbol_table.add_new_label(label, instr_num),
-            _ => {}
-        }
-    }
-
-    for line in &lines {
-        let parsed_line = HackLine::parse_line(&line)?;
-        #[cfg(debug_assertions)]
-        println!("{} -> {:?}", line, parsed_line);
-        if let Some(bincode) = to_binary::binary_of(parsed_line, &mut symbol_table) {
-            writeln!(writer, "{}", bincode)?;
-        }
+    if let Err(e) = assemble(&mut writer, &lines, &mut symbol_table) {
+        std::fs::remove_file(args.output.unwrap())?;
+        return Err(e);
     }
 
     Ok(())
